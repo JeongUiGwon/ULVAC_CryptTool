e977e6adab879ee317aee34d036f61305c7b76d0
UiGwon Jeong <jek9412@naver.com>
2026-03-02 17:03:34 +0900

    Folder Encrypt/Decrypt
    Related Issue : #1

---
+++ CryptTool/Services/FileSystemService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CryptTool.Services
+{
+    public class FileSystemService : IFileSystemService
+    {
+        public string BrowseFile()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "All Files (*.*)|*.*";
+            dlg.Multiselect = false;
+
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                return dlg.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public IList<string> GetFiles(string folderPath, bool includeSubfolders, string searchPattern)
+        {
+            if (string.IsNullOrEmpty(folderPath))
+            {
+                throw new ArgumentException("folderPath");
+            }
+
+            if (!Directory.Exists(folderPath))
+            {
+                throw new DirectoryNotFoundException(folderPath);
+            }
+
+            string pattern = string.IsNullOrEmpty(searchPattern)
+                             ? "*.*"
+                             : searchPattern;
+
+            SearchOption option = includeSubfolders
+                                  ? SearchOption.AllDirectories
+                                  : SearchOption.TopDirectoryOnly;
+
+            string[] files = Directory.GetFiles(folderPath, pattern, option);
+
+            return new List<string>(files);
+        }
+    }
+}
---
+++ CryptTool/Services/FolderSystemService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CryptTool.Services
+{
+    public class FolderSystemService : IFolderSystemService
+    {
+        public string BrowseFolder()
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.ShowNewFolderButton = true;
+
+                DialogResult result = dialog.ShowDialog();
+
+                if (result != DialogResult.OK)
+                {
+                    return null;
+                }
+
+                return dialog.SelectedPath;
+            }
+        }
+    }
+}
